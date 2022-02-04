3ed414508dbccdb794d005e39b1a45f1141922d1
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2022-02-04 18:06:09 +0100

    Integrate with firebase :fire:

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import {
-  Component,
-  createEffect,
-  createResource,
-  createSignal,
-  lazy,
-  Show,
-} from "solid-js";
+import { Component, lazy } from "solid-js";
 
 import styles from "./App.module.css";
-import CoinList from "./components/CoinList/CoinList";
-import { Coin } from "./components/CoinLine/CoinLine";
-import { SearchBar } from "./components/SearchBar/SearchBar";
 import AppBar from "./components/AppBar/AppBar";
-import { Routes, useRoutes } from "solid-app-router";
+import { useRoutes } from "solid-app-router";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBWOrhkkZ8Rp9ZiSylQJXuo4qiD-2H04ls",
+  authDomain: "solid-crypto.firebaseapp.com",
+  projectId: "solid-crypto",
+  storageBucket: "solid-crypto.appspot.com",
+  messagingSenderId: "514188354466",
+  appId: "1:514188354466:web:cbc727e7f4623291fc48fb",
+  measurementId: "G-ZETFG854DQ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const routes = [
   {
