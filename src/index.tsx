e8d49f97167a9666d5f7e4f027f69735f368194e
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2022-02-04 15:52:06 +0100

    Add solid router :wrench:

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { Router } from "solid-app-router";
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById("root") as HTMLElement
+);
