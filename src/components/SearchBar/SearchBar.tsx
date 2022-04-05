64022dc1c76496f7c05e59000fcd3a8427aae02b
Julio Valls <julioevm@gmail.com>
2022-04-05 16:06:44 +0200

    Fix reactivity issue.

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,14 @@
 import styles from './SearchBar.module.css';
 
-export const SearchBar = (props: {handleChange: (e: any) => void}) => {
+export const SearchBar = (props: { handleChange: (e: any) => void }) => {
   return (
-    <div className={styles.coin_search}>
-      <input className={styles.coin_input} type="text" placeholder='Search...' onInput={props.handleChange}/>
+    <div class={styles.coin_search}>
+      <input
+        class={styles.coin_input}
+        type="text"
+        placeholder="Search..."
+        onInput={(e) => props.handleChange(e)}
+      />
     </div>
   );
 };
